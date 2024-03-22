87bb1b347751b094f212fec2208e418bb00a2109
intwari kagame fred <intwari.k.fred@gmail.com>
2024-03-22 02:33:13 -0700

    State is tied to a position in the render tree

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [showB, setShowB] = useState(true);
+  const [isFancy, setIsFancy] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+  const [isPlayerA, setIsPlayerA] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Counter isFancy={isFancy} /> {/* Pass isFancy as a prop */}
+      {showB && <Counter isFancy={isFancy} />} {/* Pass isFancy as a prop */}
+      <label>
+        <input
+          type="checkbox"
+          checked={showB}
+          onChange={(e) => {
+            setShowB(e.target.checked);
+          }}
+        />
+        Render the second counter
+      </label>
+
+      {isFancy ? (
+        <Counter isFancy={true} />
+      ) : (
+        <Counter isFancy={false} />
+      )}
+      <label>
+        <input
+          type="checkbox"
+          checked={isFancy}
+          onChange={(e) => {
+            setIsFancy(e.target.checked);
+          }}
+        />
+        Use fancy styling
+      </label>
+
+      {isPaused ? (
+        <p>See you later!</p>
+      ) : (
+        <Counter />
+      )}
+      <label>
+        <input
+        type="checkbox"
+        checked={isPaused}
+        onChange={e => {
+          setIsPaused(e.target.checked)
+        }}
+        />
+        Take a break
+      </label>
+
+      {isPlayerA &&
+        <Counter2 person="Taylor"/>
+      } 
+
+        {!isPlayerA &&
+        <Counter2 person="Sarah"/>
+        } 
+      <button onClick={() => {
+        setIsPlayerA(!isPlayerA);
+      }}>Next Player!</button>
     </div>
   );
 }
 
-export default App;
+function Counter({ isFancy}) { // Receive isFancy as a prop
+  const [score, setScore] = useState(0);
+  const [hover, setHover] = useState(false);
+
+  let className = 'counter';
+  if (hover) {
+    className += ' hover';
+  }
+  if (isFancy) {
+    className += ' fancy';
+  }
+
+  return (
+    <div
+      className={className}
+      onPointerEnter={() => setHover(true)}
+      onPointerLeave={() => setHover(false)}
+    >
+      
+      <h1>{score}</h1>
+      <button onClick={() => setScore(score + 1)}>Add one</button>
+    </div>
+  );
+}
+
+function Counter2({ person}) { // Receive isFancy as a prop
+  const [score, setScore] = useState(0);
+  const [hover, setHover] = useState(false);
+
+  let className = 'counter';
+  if (hover) {
+    className += ' hover';
+  }
+  
+
+  return (
+    <div
+      className={className}
+      onPointerEnter={() => setHover(true)}
+      onPointerLeave={() => setHover(false)}
+    >
+      
+      <h1>{person}'s score: {score}</h1>
+      <button onClick={() => setScore(score + 1)}>Add one</button>
+    </div>
+  );
+}
+
+
